debb81ceaa0318766c162066f0824dbd218941e9
Ritu <ritudaliya1998@gmail.com>
2022-02-22 18:41:06 +0530

    Custom Payment

---
+++ view/frontend/web/js/view/payment/method-renderer/custompayment.js
@@ -0,0 +1,14 @@
+define(
+    [
+        'Magento_Checkout/js/view/payment/default'
+    ],
+    function (Component) {
+        'use strict';
+ 
+        return Component.extend({
+            defaults: {
+                template: 'Dotsquares_CustomPayment/payment/customtemplate'
+            }
+        });
+    }
+);
